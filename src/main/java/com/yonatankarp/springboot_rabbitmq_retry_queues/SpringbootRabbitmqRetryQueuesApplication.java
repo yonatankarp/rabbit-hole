67ab94197ec590478d83126d36d32eaa0f03c983
yonatankarp <yonatan.karp@sumup.com>
2021-03-20 08:33:18 +0100

    Apply spotless changes to the repository

--- src/main/java/com/yonatankarp/springboot_rabbitmq_retry_queues/SpringbootRabbitmqRetryQueuesApplication.java
+++ src/main/java/com/yonatankarp/springboot_rabbitmq_retry_queues/SpringbootRabbitmqRetryQueuesApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootRabbitmqRetryQueuesApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootRabbitmqRetryQueuesApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootRabbitmqRetryQueuesApplication.class, args);
+  }
 }
