7d9f669bf66597dd11fccccf1908c92c760ced70
Yonatan Karp-Rudin <yonatan.karp@sumup.com>
2021-12-22 19:01:18 +0200

    Merge the demo-app repo into the library repo
    This commit merging the demo application into this repository and deprecating the previous repository, as it was redundant

---
+++ demo-app/src/test/java/com/yonatankarp/rabbitholedemoapplication/RabbitHoleDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yonatankarp.rabbitholedemoapplication;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+class RabbitHoleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
