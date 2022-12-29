247b366aae6b5222a499f2e1ff2c4b6e4869502d
Yonatan Karp-Rudin <yonatan.karp@sumup.com>
2022-12-29 09:07:44 +0100

    Migrate to Gradle Kotlin DSL
    This commit migrate to Kotlin DSL of the gradle script, and update the `buildSrc` to match the rest of my repositories standard

---
+++ rabbit-hole/build.gradle.kts
@@ -0,0 +1,49 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+    id("java")
+    kotlin("jvm")
+    kotlin("plugin.allopen")
+    id("jacoco")
+    id("maven-publish")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("com.diffplug.spotless")
+}
+
+tasks {
+    getByName<Jar>("jar") {
+        enabled = true
+        archiveBaseName.set("")
+    }
+
+    getByName<BootJar>("bootJar") {
+        enabled = false
+    }
+
+    build {
+        finalizedBy(spotlessApply)
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+        finalizedBy(spotlessApply)
+        finalizedBy(jacocoTestReport)
+    }
+}
+
+dependencies {
+    implementation(group = "org.jetbrains.kotlin", name = "kotlin-stdlib-jdk8")
+    implementation(group = "org.springframework.boot", name = "spring-boot-starter-amqp")
+
+    // Tests
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+    testImplementation(group = "io.mockk", name = "mockk", version = "1.13.2")
+
+}
+
+allOpen {
+    annotation("org.springframework.context.annotation.Configuration")
+}
